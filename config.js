650ab337accd4c261ed687ed441a73f47e2efca6
abdellah bengriche <abdellah_bengriche.fs@univ-boumerdes.dz>
2026-02-13 10:28:50 +0100

    Update valentineName and celebration messages

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    valentineName: "mili"
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
@@ -22,7 +22,7 @@ const CONFIG = {
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "Do you like me?",                                    // First interaction
+            text: "Do you love me?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
             secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
@@ -49,8 +49,8 @@ const CONFIG = {
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        title: "my sweet lil potato ! 🎉💝💖💝💓",
+        message: "Now come get your gift,(ik its not money but this what i can do 👀 )!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
